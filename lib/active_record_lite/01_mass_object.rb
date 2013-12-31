a2ca0d8b9b4bdaa115972530f014207190e6bcc2
Kimberly Narine <kimberlyonarine@gmail.com>
2013-12-31 12:10:15 -0500

    my_attr_accessible allows mass assignment

--- lib/active_record_lite/01_mass_object.rb
+++ lib/active_record_lite/01_mass_object.rb
@@ -2,14 +2,24 @@ require_relative '00_attr_accessor_object.rb'
 
 class MassObject < AttrAccessorObject
   def self.my_attr_accessible(*new_attributes)
-    # ...
+    self.attributes.concat(new_attributes)
   end
 
   def self.attributes
-    # ...
+    if self == MassObject
+      raise 'must not call #attributes on MassObject directly'
+    end
+    @attributes ||= []
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |name, value|
+      if self.class.attributes.include?(name.to_sym)
+        instance_variable_set("@#{name}".to_sym, value)
+      else
+        raise "mass assignment to unregistered attribute '#{name}'"
+      end
+    end
   end
+  
 end
