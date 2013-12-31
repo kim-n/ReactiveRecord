81addbdf32725ac7b27aef3b4350d7f890ce1d90
Kimberly Narine <kimberlyonarine@gmail.com>
2013-12-31 12:07:59 -0500

    macro defines getters & settters given a list of symbols as method names

--- lib/active_record_lite/00_attr_accessor_object.rb
+++ lib/active_record_lite/00_attr_accessor_object.rb
@@ -1,5 +1,9 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      instance_name = "@#{name.to_s}".to_sym
+      define_method(name){ self.instance_variable_get(instance_name) }
+      define_method("#{name.to_s}=".to_sym){ |val| self.instance_variable_set(instance_name, val) }      
+    end
   end
 end
