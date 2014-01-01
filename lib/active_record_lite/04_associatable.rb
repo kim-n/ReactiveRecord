74667fa098c34f756c3e78e889f41ed3d2b6114f
Kimberly Narine <kimberlyonarine@gmail.com>
2014-01-01 15:31:29 -0500

    Replicates ActiveRecord's belongs_to & has_many associations

--- lib/active_record_lite/04_associatable.rb
+++ lib/active_record_lite/04_associatable.rb
@@ -10,34 +10,50 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
+  
   def initialize(name, options = {})
-    # ...
+    self.foreign_key = options[:foreign_key] || "#{name.to_s}_id".to_sym
+    self.primary_key = options[:primary_key] || :id
+    self.class_name = options[:class_name] || "#{name.to_s.capitalize}"
   end
 end
 
 class HasManyOptions < AssocOptions
+  
   def initialize(name, self_class_name, options = {})
-    # ...
+    self.class_name = options[:class_name] || "#{name.to_s.capitalize.singularize}"
+    self.foreign_key = options[:foreign_key] || "#{self_class_name.downcase}_id".to_sym
+    self.primary_key = options[:primary_key] || :id
   end
 end
 
 module Associatable
   # Phase IVb
   def belongs_to(name, options = {})
-    # ...
+    opts = BelongsToOptions.new(name, options)
+    foreign_key = opts.foreign_key
+    self.send(:define_method, name.to_s) do
+      opts.model_class.where( :id => self.send(foreign_key)).first
+    end
+    
   end
 
   def has_many(name, options = {})
-    # ...
+    opts = HasManyOptions.new(name, self.name , options)
+    foreign_key = opts.foreign_key
+    self.send(:define_method, name.to_s) do
+      opts.model_class.where( foreign_key => self.id)
+    end
+    
   end
 
   def assoc_options
@@ -46,5 +62,6 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  
+  extend Associatable
 end
