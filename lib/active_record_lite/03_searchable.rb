065a9ed060be67e3e9288faf57d44d4d8b6c08a1
Kimberly Narine <kimberlyonarine@gmail.com>
2014-01-01 14:00:52 -0500

    Replicates ActiveRecord's where method

--- lib/active_record_lite/03_searchable.rb
+++ lib/active_record_lite/03_searchable.rb
@@ -3,10 +3,46 @@ require_relative '02_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    values = []
+    where_line = []
+    params.map do |key, value|
+      values << [value]
+      where_line << "#{key}=?"
+    end
+    
+    where_line = where_line.join(" AND ")
+    
+    results = DBConnection.execute(<<-SQL, *values )
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_line}
+    SQL
+    
+    parse_all(results)
   end
 end
 
 class SQLObject
+  extend Searchable
   # Mixin Searchable here...
 end
+
+
+# TESTE
+
+class Cat < SQLObject
+  my_attr_accessor :id, :name, :owner_id
+  my_attr_accessible :id, :name, :owner_id
+end
+
+class Human < SQLObject
+  self.table_name = "humans"
+
+  my_attr_accessor :id, :fname, :lname, :house_id
+  my_attr_accessible :id, :fname, :lname, :house_id
+end
+
+Cat.where(:name => "Breakfast")
